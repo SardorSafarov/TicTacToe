0af9dec0f291f672d1f36600b4349dd4fa21d3da
SardorSafarov <zomin000s@gmail.com>
2022-07-26 00:43:04 -0700

    fun done

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.tictactoe
 
+import android.content.DialogInterface
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.example.tictactoe.databinding.ActivityMainBinding
+import com.example.tictactoe.databinding.DialogBinding
+import com.example.tictactoe.log.D
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var isXTurn = true
-    val array = Array(3){CharArray(3){'*'} }
+    var stop = 1
+    val array = Array(3) { CharArray(3) { '*' } }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,78 +28,136 @@ class MainActivity : AppCompatActivity() {
         setBtnClicListener()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun setBtnClicListener() {
         binding.apply {
             btn1.setOnClickListener {
-                setX(btn1,1)
+                setX(btn1, 1)
             }
             btn2.setOnClickListener {
-                setX(btn2,2)
+                setX(btn2, 2)
             }
             btn3.setOnClickListener {
-                setX(btn3,3)
+                setX(btn3, 3)
             }
             btn4.setOnClickListener {
-                setX(btn4,4)
+                setX(btn4, 4)
             }
             btn5.setOnClickListener {
-                setX(btn5,5)
+                setX(btn5, 5)
             }
             btn6.setOnClickListener {
-                setX(btn6,6)
+                setX(btn6, 6)
             }
             btn7.setOnClickListener {
-                setX(btn7,7)
+                setX(btn7, 7)
             }
             btn8.setOnClickListener {
-                setX(btn8,8)
+                setX(btn8, 8)
             }
             btn9.setOnClickListener {
-                setX(btn9,9)
+                setX(btn9, 9)
             }
         }
     }
 
-   private fun setX(btn: Button,n:Int) {
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setX(btn: Button, n: Int) {
+        if (stop == 9) {
+            showWinDiaolg("Durrang!!")
+        }
+        stop++
         if (btn.text.isNotEmpty()) return
         btn.text = if (isXTurn) {
-            btn.setTextColor(Color.RED)
-            array[(n-1)/3][(n-1)%3] = 'X'
-            "X"
+            btn.setTextColor(getColor(R.color.x_color))
+            array[(n - 1) / 3][(n - 1) % 3] = 'X'
+            getString(R.string.x)
         } else {
-            btn.setTextColor(Color.GREEN)
-            array[(n-1)/3][(n-1)%3] = '0'
-            "O"
+            btn.setTextColor(getColor(R.color.o_color))
+            array[(n - 1) / 3][(n - 1) % 3] = '0'
+            getString(R.string.o)
         }
         binding.textView.text =
             if (isXTurn) getString(R.string.o_navbati) else getString(R.string.x_navbati)
         isXTurn = !isXTurn
         checkWin()
     }
-    private fun checkWin()
-    {
-        if(array[0][0]!='*' && array[0][0]==array[1][1] && array[1][1] == array[2][2])
-        {
-            showWinDiaolg(array[0][0])
+
+    private fun checkWin() {
+        if (array[0][0] != '*' && array[0][0] == array[1][1] && array[1][1] == array[2][2]) {
+            showWinDiaolg(array[1][1].toString())
         }
 
-        array.forEach { char->
-            if(char[0]!='*' && char[0]==char[1] && char[1]==char[2])
-            {
-                showWinDiaolg(char[0])
+        if (array[0][2] != '*' && array[0][2] == array[1][1] && array[1][1] == array[2][0]) {
+            showWinDiaolg(array[1][1].toString())
+        }
+
+        array.forEach { char ->
+            if (char[0] != '*' && char[0] == char[1] && char[1] == char[2]) {
+                showWinDiaolg(char[0].toString())
+                return
             }
         }
-        for(i in 0 until 3){
-            if(array[0][i]!='*' && array[0][i]==array[1][i] && array[1][i]==array[2][i])
-            {
-                showWinDiaolg(array[0][i])
+        for (i in 0 until 3) {
+            if (array[0][i] != '*' && array[0][i] == array[1][i] && array[1][i] == array[2][i]) {
+                showWinDiaolg(array[0][i].toString())
+                return
             }
         }
 
     }
 
-    private fun showWinDiaolg(c: Char) {
+    private fun showWinDiaolg(c: String) {
+        val dialogBinding = DialogBinding.inflate(LayoutInflater.from(this))
 
+        when (c) {
+            "Durrang!!" -> {
+                dialogBinding.textDialog.setText("Durrang bo`ldi!!")
+                binding.apply {
+                    stop.text = (stop.text.toString().toInt() + 1).toString()
+                }
+            }
+            "X" -> {
+                    dialogBinding.textDialog.setText("X Yutdi!!")
+                    binding.winX.text = (binding.winX.text.toString().toInt() + 1).toString()
+
+            }
+            "0"->{
+                dialogBinding.textDialog.setText("0 Yutdi!!")
+                binding.win0.text = (binding.win0.text.toString().toInt() + 1).toString()
+            }
+        }
+
+        val alertDialog = AlertDialog.Builder(this, R.style.DialogCustomer)
+            .setView(dialogBinding.root)
+            .setCancelable(false)
+            .create()
+
+        dialogBinding.btnDialog.setOnClickListener {
+            clearBtn()
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
+    }
+
+    private fun clearBtn() {
+        stop = 1
+        binding.apply {
+            btn1.text = ""
+            btn2.text = ""
+            btn3.text = ""
+            btn4.text = ""
+            btn5.text = ""
+            btn6.text = ""
+            btn7.text = ""
+            btn8.text = ""
+            btn9.text = ""
+        }
+        for (i in 0 until 3) {
+            for (j in 0 until 3) {
+                array[i][j] = '*'
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/tictactoe/log/D.kt
@@ -0,0 +1,9 @@
+package com.example.tictactoe.log
+
+import android.util.Log
+
+object D {
+    fun d(message:String){
+        Log.d("sardor","---->   $message   <----")
+    }
+}
